a4a5fa5f5cfe3f58cafc5fcecfe7f445532b3c5a
Thimey <simon.angell307@gmail.com>
2022-06-16 20:11:32 +1000

    Refactor larva draw

--- src/Game/SelectionDisplay.tsx
+++ src/Game/SelectionDisplay.tsx
@@ -21,15 +21,15 @@ function showLarvaSelected(state: State<BlobQueenContext>) {
   return state.matches({ ready: { itemSelection: 'larvaSelected' } });
 }
 
-const PROFILE_CANVAS_HEIGHT = 200;
+const PROFILE_CANVAS_HEIGHT = 100;
 const PROFILE_CANVAS_WIDTH = 100;
 
 const SPAWN_ITEM_HEIGHT = 25;
 const SPAWN_ITEM_WIDTH = 25;
-const LARVA_SCALE_FACTOR = 3;
+const LARVA_SCALE_FACTOR = 2;
 
 interface BlobSpawnItem {
-  type: BlobSpawn;
+  type: BlobSpawn | 'unknown';
   draw: (opts: DrawOptions) => void;
 }
 
@@ -37,13 +37,14 @@ const larvaBodyRadiusX = BLOB_LARVA_BODY_RADIUS_X * LARVA_SCALE_FACTOR;
 const larvaBodyRadiusY = BLOB_LARVA_BODY_RADIUS_Y * LARVA_SCALE_FACTOR;
 const larvaHeadRadius = BLOB_LARVA_HEAD_RADIUS * LARVA_SCALE_FACTOR;
 const larvaX = larvaBodyRadiusX + larvaHeadRadius;
+const larvaY = 2 * larvaHeadRadius;
 
 function drawLarvaProfile({ ctx }: DrawOptions) {
   ctx.beginPath();
   drawLarva(
     {
-      position: { x: larvaX, y: larvaHeadRadius },
-      destination: { x: larvaX + 1, y: larvaHeadRadius },
+      position: { x: larvaX, y: larvaY },
+      destination: { x: larvaX + 1, y: larvaY },
       larvaBodyRadiusX,
       larvaBodyRadiusY,
       larvaHeadRadius,
@@ -65,11 +66,19 @@ function drawBlobletSpawn({ ctx, canvasHeight, canvasWidth }: DrawOptions) {
   ctx.closePath();
 }
 
+function drawUnknown({ ctx, canvasHeight, canvasWidth }: DrawOptions) {
+  ctx.beginPath();
+  ctx.font = '20px Arial';
+  ctx.fillStyle = 'black';
+  ctx.fillText('?', canvasWidth / 2, canvasHeight);
+  ctx.closePath();
+}
+
 const spawnSelection: BlobSpawnItem[] = [
   { type: 'bloblet', draw: drawBlobletSpawn },
-  { type: 'bloblet', draw: drawBlobletSpawn },
-  { type: 'bloblet', draw: drawBlobletSpawn },
-  { type: 'bloblet', draw: drawBlobletSpawn },
+  { type: 'unknown', draw: drawUnknown },
+  { type: 'unknown', draw: drawUnknown },
+  { type: 'unknown', draw: drawUnknown },
 ];
 
 export interface Props {
@@ -83,7 +92,8 @@ export const SelectionDisplay = ({ blobQueenService }: Props) => {
     blobQueenService.send({ type: 'SPAWN_BLOB_SELECTED', blobToSpawn });
   };
 
-  if (showLarva) {
+  // if (showLarva) {
+  if (true) {
     return (
       <div className="fixed bottom-0 left-0">
         <h3>Larva</h3>
@@ -100,7 +110,9 @@ export const SelectionDisplay = ({ blobQueenService }: Props) => {
               <div
                 key={i}
                 className="border border-black border-solid w-10 h-10 m-1"
-                onClick={() => handleBlobSelect(type)}
+                onClick={() => {
+                  if (type !== 'unknown') handleBlobSelect(type);
+                }}
               >
                 <Canvas
                   height={SPAWN_ITEM_HEIGHT}
