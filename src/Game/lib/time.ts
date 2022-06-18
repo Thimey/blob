bd25aaa5141e7b711382127df75340015ea7c751
Oolay <alex.angell@my.jcu.edu.au>
2022-06-18 16:17:39 +1000

    Fix elapsedIntervals logic. Add additional test case.

--- src/Game/lib/time.ts
+++ src/Game/lib/time.ts
@@ -6,9 +6,11 @@ interface Args {
 }
 
 export function elapsedIntervals({ startAt, interval, from, to }: Args) {
-  return (
-    Math.floor((to - startAt) / interval) -
-    // Last from could be before interval startAt, so take abs to ensure it rounds to 0.
-    Math.floor(Math.abs((from - startAt) / interval))
-  );
+  const intervalsUntilTo = Math.floor((to - startAt) / interval);
+  // Last from could be before interval startAt, so take abs to ensure it rounds to 0.
+
+  const intervalsUntilFrom =
+    from - startAt > 0 ? Math.floor((from - startAt) / interval) : 0;
+
+  return intervalsUntilTo - intervalsUntilFrom;
 }
